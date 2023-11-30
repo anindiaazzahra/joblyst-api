38a5400ac097c7c326efa2ff91d16632b65a7c8b
Anindia Azzahra <123210072@student.upnyk.ac.id>
2023-11-30 10:57:47 +0700

    Add user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+class User {
+  constructor(id, username, email, password, fullName, address, phoneNumber, photoURL, createdAt, updatedAt) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.fullName = fullName;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.photoURL = photoURL;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
