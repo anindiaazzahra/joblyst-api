c635c54700233ae960a2b31efe917ba1fdef58bc
Anindia Azzahra <123210072@student.upnyk.ac.id>
2023-11-30 10:54:21 +0700

    Update user handler

---
+++ app/User/handler.js
@@ -0,0 +1,36 @@
+'use strict';
+const User = require('../../models/user');
+const firebase = require('../../db');
+const firestore = firebase.firestore();
+
+module.exports = {
+  handlerGetDetailUser: async (req, res, next) => {
+    try {
+      
+      
+    } catch (error) {
+      next(error);
+    }
+  },
+  handlerUpdateUserProfile: async (req, res, next) => {
+    try {
+      
+    } catch (error) {
+      next(error);
+    }
+  },
+  handlerUpdatePhotoProfile: async (req, res, next) => {
+    try {
+      
+    } catch (error) {
+      next(error);
+    }
+  },
+  handlerDeletePhotoProfile: async (req, res, next) => {
+    try {
+      
+    } catch (error) {
+      next(error);
+    }
+  }
+};
