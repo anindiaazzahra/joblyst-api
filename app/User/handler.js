881247998346a34985dd4ab37c47e38de69c64e6
Anindia Azzahra <123210072@student.upnyk.ac.id>
2023-12-01 15:26:23 +0700

    Update handler get user profile

--- app/User/handler.js
+++ app/User/handler.js
@@ -4,9 +4,26 @@ const firebase = require('../../db');
 const firestore = firebase.firestore();
 
 module.exports = {
-  handlerGetDetailUser: async (req, res, next) => {
+  handlerGetUserProfile: async (req, res, next) => {
     try {
+      const { email } = req.user;
       
+      const userSnapshot = await firestore.collection('users').where('email', '==', email).get();
+
+      if (userSnapshot.empty) {
+        return res.status(404).send({ status: 'error', message: 'User not found' });
+      }
+
+      let userData;
+      userSnapshot.forEach((doc) => {
+        userData = doc.data();
+      });
+
+      return res.status(200).send({
+        status: 'success',
+        message: 'Successfully get user data',
+        user: userData,
+      });
       
     } catch (error) {
       next(error);
--- app/User/route.js
+++ app/User/route.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { handlerGetDetailUser, handlerUpdateUserProfile, handlerUpdatePhotoProfile, handlerDeletePhotoProfile } = require("./handler");
+const { 
+  handlerGetUserProfile, 
+  handlerUpdateUserProfile, 
+  handlerUpdatePhotoProfile, 
+  handlerDeletePhotoProfile 
+} = require("./handler");
 const router = express.Router();
+const authenticationToken = require("../../middleware/authenticationToken");
 
-router.get("/profile", handlerGetDetailUser);
+router.get("/profile", authenticationToken, handlerGetUserProfile);
 router.put("/profile", handlerUpdateUserProfile);
 router.put("/photoprofile", handlerUpdatePhotoProfile);
 router.delete("/photoprofile", handlerDeletePhotoProfile);
